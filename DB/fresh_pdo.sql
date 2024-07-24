d1922dcc971954401c9b84fe526bcf9229b11f98
Shahensha Alam <64788000+mr-shahensha@users.noreply.github.com>
2024-07-24 10:34:17 +0530

    Update fresh_pdo.sql

--- DB/fresh_pdo.sql
+++ DB/fresh_pdo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 23, 2024 at 08:11 AM
+-- Generation Time: Jul 24, 2024 at 07:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
